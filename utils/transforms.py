59a4de6b85cde795fa0c3d05c5e03ab827ee2124
IsHYuhi <yuhiregend@icloud.com>
2019-12-30 18:14:15 +0900

    add ColorJitter

--- utils/transforms.py
+++ utils/transforms.py
@@ -3,6 +3,7 @@ import torch
 import math
 import warnings
 import numbers
+from torchvision import transforms
 from torchvision.transforms import functional as F
 
 def _flip_coco_person_keypoints(kps, width):
@@ -150,94 +151,125 @@ class RandomErasing(object):
         return img, target
 
 
-# class ColorJitter(object):
-#     """Randomly change the brightness, contrast and saturation of an image.
+class ColorJitter(object):
+    """Randomly change the brightness, contrast and saturation of an image.
 
-#     Args:
-#         brightness (float or tuple of float (min, max)): How much to jitter brightness.
-#             brightness_factor is chosen uniformly from [max(0, 1 - brightness), 1 + brightness]
-#             or the given [min, max]. Should be non negative numbers.
-#         contrast (float or tuple of float (min, max)): How much to jitter contrast.
-#             contrast_factor is chosen uniformly from [max(0, 1 - contrast), 1 + contrast]
-#             or the given [min, max]. Should be non negative numbers.
-#         saturation (float or tuple of float (min, max)): How much to jitter saturation.
-#             saturation_factor is chosen uniformly from [max(0, 1 - saturation), 1 + saturation]
-#             or the given [min, max]. Should be non negative numbers.
-#         hue (float or tuple of float (min, max)): How much to jitter hue.
-#             hue_factor is chosen uniformly from [-hue, hue] or the given [min, max].
-#             Should have 0<= hue <= 0.5 or -0.5 <= min <= max <= 0.5.
-#     """
-#     def __init__(self, brightness=0, contrast=0, saturation=0, hue=0):
-#         self.brightness = self._check_input(brightness, 'brightness')
-#         self.contrast = self._check_input(contrast, 'contrast')
-#         self.saturation = self._check_input(saturation, 'saturation')
-#         self.hue = self._check_input(hue, 'hue', center=0, bound=(-0.5, 0.5),
-#                                      clip_first_on_zero=False)
+    Args:
+        brightness (float or tuple of float (min, max)): How much to jitter brightness.
+            brightness_factor is chosen uniformly from [max(0, 1 - brightness), 1 + brightness]
+            or the given [min, max]. Should be non negative numbers.
+        contrast (float or tuple of float (min, max)): How much to jitter contrast.
+            contrast_factor is chosen uniformly from [max(0, 1 - contrast), 1 + contrast]
+            or the given [min, max]. Should be non negative numbers.
+        saturation (float or tuple of float (min, max)): How much to jitter saturation.
+            saturation_factor is chosen uniformly from [max(0, 1 - saturation), 1 + saturation]
+            or the given [min, max]. Should be non negative numbers.
+        hue (float or tuple of float (min, max)): How much to jitter hue.
+            hue_factor is chosen uniformly from [-hue, hue] or the given [min, max].
+            Should have 0<= hue <= 0.5 or -0.5 <= min <= max <= 0.5.
+    """
+    def __init__(self, p=0.5, brightness=0, contrast=0, saturation=0, hue=0):
+        self.p = p
+        self.brightness = self._check_input(brightness, 'brightness')
+        self.contrast = self._check_input(contrast, 'contrast')
+        self.saturation = self._check_input(saturation, 'saturation')
+        self.hue = self._check_input(hue, 'hue', center=0, bound=(-0.5, 0.5),
+                                     clip_first_on_zero=False)
 
-#     def _check_input(self, value, name, center=1, bound=(0, float('inf')), clip_first_on_zero=True):
-#         if isinstance(value, numbers.Number):
-#             if value < 0:
-#                 raise ValueError("If {} is a single number, it must be non negative.".format(name))
-#             value = [center - value, center + value]
-#             if clip_first_on_zero:
-#                 value[0] = max(value[0], 0)
-#         elif isinstance(value, (tuple, list)) and len(value) == 2:
-#             if not bound[0] <= value[0] <= value[1] <= bound[1]:
-#                 raise ValueError("{} values should be between {}".format(name, bound))
-#         else:
-#             raise TypeError("{} should be a single number or a list/tuple with lenght 2.".format(name))
+    def _check_input(self, value, name, center=1, bound=(0, float('inf')), clip_first_on_zero=True):
+        if isinstance(value, numbers.Number):
+            if value < 0:
+                raise ValueError("If {} is a single number, it must be non negative.".format(name))
+            value = [center - value, center + value]
+            if clip_first_on_zero:
+                value[0] = max(value[0], 0)
+        elif isinstance(value, (tuple, list)) and len(value) == 2:
+            if not bound[0] <= value[0] <= value[1] <= bound[1]:
+                raise ValueError("{} values should be between {}".format(name, bound))
+        else:
+            raise TypeError("{} should be a single number or a list/tuple with lenght 2.".format(name))
 
-#         # if value is 0 or (1., 1.) for brightness/contrast/saturation
-#         # or (0., 0.) for hue, do nothing
-#         if value[0] == value[1] == center:
-#             value = None
-#         return value
+        # if value is 0 or (1., 1.) for brightness/contrast/saturation
+        # or (0., 0.) for hue, do nothing
+        if value[0] == value[1] == center:
+            value = None
+        return value
 
-#     @staticmethod
-#     def get_params(brightness, contrast, saturation, hue):
-#         """Get a randomized transform to be applied on image.
+    @staticmethod
+    def get_params(brightness, contrast, saturation, hue):
+        """Get a randomized transform to be applied on image.
 
-#         Arguments are same as that of __init__.
+        Arguments are same as that of __init__.
 
-#         Returns:
-#             Transform which randomly adjusts brightness, contrast and
-#             saturation in a random order.
-#         """
-#         transforms = []
+        Returns:
+            Transform which randomly adjusts brightness, contrast and
+            saturation in a random order.
+        """
+        transforms = []
 
-#         if brightness is not None:
-#             brightness_factor = random.uniform(brightness[0], brightness[1])
-#             transforms.append(Lambda(lambda img: F.adjust_brightness(img, brightness_factor)))
+        if brightness is not None:
+            brightness_factor = random.uniform(brightness[0], brightness[1])
+            transforms.append(Lambda(lambda img: F.adjust_brightness(img, brightness_factor)))
 
-#         if contrast is not None:
-#             contrast_factor = random.uniform(contrast[0], contrast[1])
-#             transforms.append(Lambda(lambda img: F.adjust_contrast(img, contrast_factor)))
+        if contrast is not None:
+            contrast_factor = random.uniform(contrast[0], contrast[1])
+            transforms.append(Lambda(lambda img: F.adjust_contrast(img, contrast_factor)))
 
-#         if saturation is not None:
-#             saturation_factor = random.uniform(saturation[0], saturation[1])
-#             transforms.append(Lambda(lambda img: F.adjust_saturation(img, saturation_factor)))
+        if saturation is not None:
+            saturation_factor = random.uniform(saturation[0], saturation[1])
+            transforms.append(Lambda(lambda img: F.adjust_saturation(img, saturation_factor)))
 
-#         if hue is not None:
-#             hue_factor = random.uniform(hue[0], hue[1])
-#             transforms.append(Lambda(lambda img: F.adjust_hue(img, hue_factor)))
+        if hue is not None:
+            hue_factor = random.uniform(hue[0], hue[1])
+            transforms.append(Lambda(lambda img: F.adjust_hue(img, hue_factor)))
 
-#         random.shuffle(transforms)
-#         transform = Compose(transforms)
+        random.shuffle(transforms)
+        transform = Compose(transforms)
 
-#         return transform
+        return transform
 
-#     def __call__(self, img, target):
-#         """
-#         Args:
-#             img (PIL Image): Input image.
+    def __call__(self, img, target):
+        """
+        Args:
+            img (PIL Image): Input image.
 
-#         Returns:
-#             PIL Image: Color jittered image.
-#         """
-#         transform = self.get_params(self.brightness, self.contrast,
-#                                     self.saturation, self.hue)
-#         img = transform(img)
-#         return img, target
+        Returns:
+            PIL Image: Color jittered image.
+        """
+        #transform = self.get_params(self.brightness, self.contrast,
+        #                            self.saturation, self.hue)
+        if random.uniform(0, 1) < self.p:
+            r = random.uniform(0,1)
+            img = transforms.ToPILImage()(img).convert("RGB")
+            if r <= 0.25:
+                if self.brightness is not None:
+                    brightness_factor = random.uniform(self.brightness[0], self.brightness[1])
+                    img = F.adjust_brightness(img, brightness_factor)
+                    img = F.to_tensor(img)
+                    return img, target #PILimageでないといけない
+            elif r <= 0.5:
+                if self.contrast is not None:
+                    contrast_factor = random.uniform(self.contrast[0], self.contrast[1])
+                    img = F.adjust_contrast(img, contrast_factor)
+                    img = F.to_tensor(img)
+                    return img, target
+
+            elif r <= 0.75:
+                if self.saturation is not None:
+                    saturation_factor = random.uniform(self.saturation[0], self.saturation[1])
+                    F.adjust_saturation(img, saturation_factor)
+                    img = F.to_tensor(img)
+                    return img, target
+
+            elif r <= 1.0:
+                if self.hue is not None:
+                    hue_factor = random.uniform(self.hue[0], self.hue[1])
+                    F.adjust_hue(img, hue_factor)
+                    img = F.to_tensor(img)
+                    return img, target
+
+        #img = transform(img) #TODO
+        return img, target
 
     def __repr__(self):
         format_string = self.__class__.__name__ + '('
